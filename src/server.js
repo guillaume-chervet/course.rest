a35e1062705605f2960c57b123cf67ebf3754844
Guillaume Chervet <guillaume.chervet@axa.fr>
2019-11-15 17:10:24 +0000

    wip

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-let app = express();
-var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log(`Example app listening at http://${host}:${port}`);
-});
